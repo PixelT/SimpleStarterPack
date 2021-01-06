4c2c26bd2ea8733fe59399c7acf971fdc0ae68f7
PixelT <pixeltownpl@gmail.com>
2021-01-06 22:40:32 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,29 @@
+# SSP » Simple Starter Pack
+Simple starter pack with pre-configured usefull tools & libraries:
+- Live reload & sync (using [Browsersync](https://browsersync.io/))
+- SCSS compile with autoprefixer, sourcemaps and minification
+- JS compile (using [Babel](https://babeljs.io/)) with sourcemaps and minification
+- JPG / PNG / GIF images optimization (with imagemin)
+- SVG optimization (with SVGO)
+- Predefined tasks for VSCode editor
+- Included Sass libraries:
+    - [normalize.css](https://necolas.github.io/normalize.css/) - A modern, HTML5-ready alternative to CSS resets
+    - [include-media](https://github.com/eduardoboucas/include-media/) - Simple, elegant and maintainable media queries in Sass
+- Configured packages:
+    - [JSDOM](https://quokkajs.com/docs/configuration.html#jsdom) for [Quokka.js](https://quokkajs.com/) tool
+
+## Installation & Usage
+```sh
+git clone https://github.com/PixelT/SimpleStarterPack.git path/to/your/project
+cd path/to/your/project
+yarn install
+```
+
+## Additional information
+It's possible to pass to Browsersync custom settings, by adding additional arguments into `gulp dev` task:
+`--proxy="your_proxy_address"` - custom proxy URL address (default is `<your_folder_name>.test`)
+`--port="your_port"` - custom port (default is `3000`)
+
+## Gulp tasks
+`gulp` or `gulp build` - it compiles & minifies SCSS and JS, sourcemaps are disabled.
+`gulp dev` - it runs Browsersync with sourcemaps enabled.
---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+const $ = require('gulp-load-plugins')({'pattern': '*'});
+const isDev = process.argv[process.argv.length - 1] === 'dev';
+const port = ($.yargs.argv.port) ? $.yargs.argv.port : 3000;
+const proxy = ($.yargs.argv.proxy) ? $.yargs.argv.proxy : false;
+
+let folderPath = (process.platform === 'darwin') ? __dirname.split('/') : __dirname.split('\\');
+    folderPath = folderPath[folderPath.length-1].replace(/([A-Z:\\]*[_]+)/g, '');
+
+$.gulp.task('browserSync', () => {
+    return $.browserSync.init({
+        proxy: proxy || folderPath + '.test',
+        port: port,
+        notify: true,
+        open: false
+    });
+});
+
+$.gulp.task('compileScss', () => {
+    return $.gulp.src('src/scss/style.scss')
+	.pipe($.plumber({
+		errorHandler (err) {
+			$.notify.onError({
+				title: (err) => `${err.file.replace(`${process.cwd()}/`, '')}:${err.line}:${err.column}`,
+				message: (err) => err.messageOriginal.trim(),
+				icon: $.path.join(__dirname, ".vscode/icons/icon-sass.png"),
+				sound: 'Frog',
+			})(err)
+			this.emit('end');
+		}
+	}))
+    .pipe($.if(isDev, $.sourcemaps.init()))
+    .pipe($.sass({outputStyle: 'compressed'}))
+    .pipe($.autoprefixer())
+    .pipe($.if(isDev, $.sourcemaps.write()))
+    .pipe($.gulp.dest('dist/css'))
+    .pipe($.browserSync.stream({ match: '**/*.css' }))
+});
+
+$.gulp.task('compileJs', () => {
+    return $.gulp.src('src/js/function.js')
+	.pipe($.plumber({
+		errorHandler (err) {
+			$.notify.onError({
+				title: (err) => `${err.fileName.replace(`${process.cwd()}/`, '')}:${err.loc.line}:${err.loc.column}`,
+				message: (err) => `${err.message.split('\n')[0].replace(`${err.fileName}: `, '')}`.trim(),
+				icon: $.path.join(__dirname, ".vscode/icons/icon-js.png"),
+				sound: 'Frog',
+			})(err)
+			this.emit('end');
+		}
+	}))
+    .pipe($.if(isDev, $.sourcemaps.init()))
+    .pipe($.babel({
+        presets: ['@babel/env']
+    }))
+    .pipe($.uglify())
+    .pipe($.if(isDev, $.sourcemaps.write()))
+    .pipe($.gulp.dest('dist/js'))
+    .pipe($.browserSync.stream({match: '**/*.js'}))
+});
+
+$.gulp.task('optimizeImages', () => {
+    return $.gulp.src('src/images/**/*.{png,gif,jpg}')
+    .pipe($.plumber())
+    .pipe($.imagemin({
+        verbose: true
+    }))
+    .pipe($.gulp.dest('dist/images'))
+    .pipe($.browserSync.stream({match: '**/*.{png,gif,jpg}'}))
+});
+
+$.gulp.task('optimizeSVG', () => {
+    return $.gulp.src('src/images/**/*.svg')
+    .pipe($.plumber())
+    .pipe($.svgmin())
+    .pipe($.gulp.dest('dist/images'))
+    .pipe($.browserSync.stream({match: '**/*.svg'}))
+})
+
+$.gulp.task('watch', $.gulp.parallel(['browserSync'], () => {
+    $.gulp.watch('**/*.php, **/*.html', {cwd:'./'}).on('change', $.browserSync.reload);
+    $.gulp.watch('src/js/**/*.js', {cwd: './'}, $.gulp.parallel(['compileJs']));
+    $.gulp.watch('src/scss/**/*.scss', {cwd: './'}, $.gulp.parallel(['compileScss']));
+    $.gulp.watch('src/images/**/*.{png,gif,jpg}', {cwd: './'}, $.gulp.parallel(['optimizeImages']));
+    $.gulp.watch('src/images/**/*.svg', {cwd: './'}, $.gulp.parallel(['optimizeSVG']));
+}));
+
+$.gulp.task('dev', $.gulp.series('compileScss', 'compileJs', $.gulp.parallel('optimizeImages', 'optimizeSVG'), 'watch'));
+$.gulp.task('build', $.gulp.series('compileScss', 'compileJs', $.gulp.parallel('optimizeImages', 'optimizeSVG')));
+
+$.gulp.task('default', $.gulp.series(['build']));
